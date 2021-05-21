68794f1a7de4ec7908ac1467d0ff85add95cfdb9
daprieu <daprieu@gmail.com>
2021-05-21 13:10:43 -0500

    quick fix for new registers

--- src/components/auth/Register.js
+++ src/components/auth/Register.js
@@ -38,6 +38,8 @@ export const Register = (props) => {
                 .then(res => {
                     if ("valid" in res && res.valid) {
                         localStorage.setItem("rare_user_id", res.token)
+                        localStorage.setItem("userId", res.userId)
+                        localStorage.setItem("isStaff", res.isStaff)
                         history.push("/")
                     }
                 })
