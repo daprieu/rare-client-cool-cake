2778f1b7387a7b780bba000c412b9893739a91d7
Dan Ross <daniellross@gmail.com>
2021-05-21 11:05:06 -0500

    added fetch call to deactive user. need additional data to render

--- src/components/profiles/ProfileProvider.js
+++ src/components/profiles/ProfileProvider.js
@@ -14,7 +14,6 @@ export const ProfileProvider = (props) => {
         })
             .then(response => response.json())
             .then(setProfile)
-            debugger
     }
     const getAllProfiles = () => {
         return fetch("http://localhost:8000/profiles", {
@@ -33,9 +32,25 @@ export const ProfileProvider = (props) => {
             })
     }
 
+    const deactivateProfile = userId => {
+        return fetch(`http://localhost:8000/profiles/${userId}/deactivate`, {
+            method: "PUT",
+            headers: {
+                "Authorization": `Token ${localStorage.getItem("rare_user_id")}`
+            }
+        })
+            .then(getAllProfiles)
+    }
+    // deactivate TODO:
+        // update profile serializer to return active status
+        // update profile serializer so inactive profiles are only returned to admin
+        // add conditional button on profile list to deactive user
+            // add confirmation on action
+
+
     return (
         <ProfileContext.Provider value={{
-            profile, getProfile, allProfiles, getAllProfiles
+            profile, getProfile, allProfiles, getAllProfiles, deactivateProfile
         }}>
             {props.children}
         </ProfileContext.Provider>
